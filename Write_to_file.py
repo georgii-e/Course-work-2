45a44e5566f52fa02e89a4db13e55e193d9b7177
Egor <egor.vas2004@gmail.com>
2022-05-30 23:20:25 +0300

    👵

--- Write_to_file.py
+++ Write_to_file.py
@@ -15,7 +15,7 @@ def write_to_file(sort_info, path):
     line2 = "Number of elements: " + str(len(final_lst)) + "\n"
     line3 = "Count of swaps: " + str(sort_info.get_swaps()) + "\n"
     line4 = "Count of comparisons: " + str(sort_info.get_comparisons()) + "\n"
-    line5 = "Initial array\n" + ' '.join(map(str, initial_lst)) + "\n"
-    line6 = "Final array\n" + ' '.join(map(str, final_lst)) + "\n\n"
+    line5 = "Initial array:\n" + ' '.join(map(str, initial_lst)) + "\n"
+    line6 = "Final array:\n" + ' '.join(map(str, final_lst)) + "\n\n"
     with open(path, 'at') as text_to_file:
         text_to_file.write(line1 + line2 + line3 + line4 + line5 + line6)
